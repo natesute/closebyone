040e180acccf8ee2e5c1d8f90211fca1eceeac1d
Nathan Suttie <nsut0004@student.monash.edu>
2021-10-19 23:46:43 +1100

    many changes

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import timeit
 import numpy as np
 from numba import int64, float64
 from numba.core.types import Array
@@ -10,18 +11,18 @@ RNG = np.random.default_rng(seed=0)
 def disc_num_to_bin(num_d_ref):
     num_d = np.copy(num_d_ref)
     bin_d = []
-    for att in range(len(num_d[0])):
+    for att in range(len(num_d[0])-1, -1, -1):
         col = num_d[:, att]
-        unique = np.unique(col)
-        for val in unique:
+        thresholds = np.unique(col)
+        for i in range(len(thresholds)-1, -1, -1):
             new_col = np.copy(col)
-            new_col[col <= val] = True
-            new_col[col > val] = False
+            new_col[col <= thresholds[i]] = True
+            new_col[col > thresholds[i]] = False
             bin_d.append(new_col)
-        for val in unique:
+        for i in range(0, len(thresholds)):
             new_col = np.copy(col)
-            new_col[col < val] = False
-            new_col[col >= val] = True
+            new_col[col >= thresholds[i]] = True
+            new_col[col < thresholds[i]] = False
             bin_d.append(new_col)
     bin_d = np.array(bin_d, dtype=bool).T
     return bin_d
@@ -180,29 +181,31 @@ def is_canonical(intent, new_intent, current_j):
 
 
 def search_plus_lower(intent, extent, j, bb):
-    print("plus lower\n")
+#     print("plus lower\n")
     # raise lower bound of j
     intent[j][0] += 1
     extent = get_extent(intent, extent, bb)
 
     if extent.size > 0:
         bb.max_obj = max(impact(extent, bb), bb.max_obj)
-        print("intent")
-        print(intent)
-        print("max obj")
-        print(bb.max_obj)
-        print("\n\n")
+#         print("intent")
+#         print(intent)
+#         print("max obj")
+#         print(bb.max_obj)
+#         print("\n\n")
         if bnd(extent, bb) >= bb.max_obj: # consider >. > would mean that, if peaking, peaked nodes would not be included at all.
-            print("bound accepted\n")
+#             print("bound accepted\n")
             # get closure, returns empty if not canonical
             new_intent = get_closure(extent, bb)
-            print("closure")
-            print(intent)
             # check if new intent is canonical
 
             if is_canonical(intent, new_intent, j):
-                print("is canonical\n")
+#                 print("is canonical\n")
+                #print("before closure")
+                #print(intent)
                 intent = new_intent
+                print("closure")
+                print(intent)
                 bb.num_nodes += 1
                 
                 # print(intent)
@@ -222,26 +225,28 @@ def search_plus_lower(intent, extent, j, bb):
 
 
 def search_minus_upper(intent, extent, j, bb):
-    print("minus upper\n")
+#     print("minus upper\n")
     # lower upper bound of j
     intent[j][1] -= 1
     extent = get_extent(np.copy(intent), np.copy(extent), bb)
     if extent.size > 0:
         bb.max_obj = max(impact(extent, bb), bb.max_obj)
-        print("intent")
-        print(intent)
-        print("max obj")
-        print(bb.max_obj)
-        print("\n\n")
+#         print("intent")
+#         print(intent)
+#         print("max obj")
+#         print(bb.max_obj)
+#         print("\n\n")
         #if bb.max_obj > old_max_obj:
         #    best_query = intent
         if bnd(extent, bb) >= bb.max_obj:
-            print("bound accepted\n")
+#             print("bound accepted\n")
             # get closure, returns empty if not canonical
             new_intent = get_closure(extent, bb)
 
             if is_canonical(intent, new_intent, j):
-                print("is canonical\n")
+#                 print("is canonical\n")
+                #print("before closure")
+                #print(intent)
                 intent = new_intent
                 print("closure")
                 print(intent)
@@ -260,7 +265,7 @@ def search_minus_upper(intent, extent, j, bb):
 
 # search changes on jth attribute downwards
 def search(intent, extent, j, bb):
-    print("next attribute down\n")
+#     print("next attribute down\n")
     if intent[j][0] != intent[j][1]:
         # search aug children of minus-ing upper bound
         search_minus_upper(np.copy(intent), np.copy(extent), j, bb)
@@ -278,8 +283,136 @@ def search(intent, extent, j, bb):
         # search changes on j-1th attribute downwards
         search(np.copy(intent), np.copy(extent), j - 1, bb)
 
-
 def run_cbo(data, target):
+    extent = np.arange(len(data))
+    bb = BB(0, 0, np.NINF, 0, 0, len(data), data, target, None)
+    bb.p0 = p(extent, bb)
+    bb.bnd0 = bnd(extent, bb)
+    dims = len(data[0])
+    # print(bb)
+    # intent = get_root(dims)
+    print("start")
+    print(extent)
+    print("data")
+    print(bb.data)
+    intent = get_closure(extent, bb)
+    extent = get_extent(np.copy(intent), np.copy(extent), bb)
+    bb.max_obj = max(impact(extent, bb), bb.max_obj)
+    print("closure")
+    print(intent)
+#     print("max obj")
+#     print(bb.max_obj)
+#     print("\n\n")
+
+    # check if inital objective value is maximum bound
+    if bb.max_obj == bb.bnd0:
+        return bb
+
+    # intent = get_closure(extent, bb)
+    bb.num_nodes += 1
+    # print(intent)
+
+    # get index of last attribute
+
+    # starts from last attribute (index dims-1) and decrements
+    j = dims-1
+    search(np.copy(intent), np.copy(extent), j, bb)
+
+    return bb
+
+
+def search_plus_lower_nc(intent, extent, j, bb):
+#     print("plus lower\n")
+    # raise lower bound of j
+    intent[j][0] += 1
+    extent = get_extent(intent, extent, bb)
+
+    if extent.size > 0:
+        bb.max_obj = max(impact(extent, bb), bb.max_obj)
+#         print("intent")
+#         print(intent)
+#         print("max obj")
+#         print(bb.max_obj)
+#         print("\n\n")
+        if bnd(extent, bb) > bb.max_obj: # consider >. > would mean that, if peaking, peaked nodes would not be included at all.
+#             print("bound accepted\n")
+#             print("closure")
+#             print(intent)
+            # check if new intent is canonical
+            intent = get_closure(extent, bb)
+            bb.num_nodes += 1
+            
+            # print(intent)
+            # check if bounds can be further changed on j
+            if intent[j][0] != intent[j][1]:
+                # branch current attribute, both bounds
+                search_minus_upper_nc(np.copy(intent), np.copy(extent), j, bb)
+                # check if maximum bound has been hit
+                if bb.max_obj == bb.bnd0:
+                    return
+                search_plus_lower_nc(np.copy(intent), np.copy(extent), j, bb)
+
+                if bb.max_obj == bb.bnd0:
+                    return
+            if j:
+                search_nc(np.copy(intent), np.copy(extent), j - 1, bb)
+
+
+def search_minus_upper_nc(intent, extent, j, bb):
+#     print("minus upper\n")
+    # lower upper bound of j
+    intent[j][1] -= 1
+    extent = get_extent(np.copy(intent), np.copy(extent), bb)
+    if extent.size > 0:
+        bb.max_obj = max(impact(extent, bb), bb.max_obj)
+#         print("intent")
+#         print(intent)
+#         print("max obj")
+#         print(bb.max_obj)
+#         print("\n\n")
+        #if bb.max_obj > old_max_obj:
+        #    best_query = intent
+        if bnd(extent, bb) > bb.max_obj:
+#             print("bound accepted\n")
+            # get closure, returns empty if not canonical
+            intent = get_closure(extent, bb)
+#             print("closure")
+#             print(intent)
+            
+            bb.num_nodes += 1
+            # check if bounds can be further changed on j
+            if intent[j][0] != intent[j][1]:
+                # branch current attribute, only upper bound
+                search_minus_upper_nc(np.copy(intent), np.copy(extent), j, bb)
+                # check if maximum bound has been hit
+                if bb.max_obj == bb.bnd0:
+                    return
+
+            if j:  # because j==0 is value col
+                search(np.copy(intent), np.copy(extent), j - 1, bb)
+
+# search changes on jth attribute downwards
+def search_nc(intent, extent, j, bb):
+#     print("next attribute down\n")
+    if intent[j][0] != intent[j][1]:
+        # search aug children of minus-ing upper bound
+        search_minus_upper_nc(np.copy(intent), np.copy(extent), j, bb)
+        # check if maximum bound has been hit
+        if bb.max_obj == bb.bnd0:
+            return
+
+        # search aug children of plus-ing lower bound
+        search_plus_lower_nc(np.copy(intent), np.copy(extent), j, bb)
+        # check if maximum bound has been hit
+        if bb.max_obj == bb.bnd0:
+            return
+
+    if j > 0:
+        # search changes on j-1th attribute downwards
+        search_nc(np.copy(intent), np.copy(extent), j - 1, bb)
+
+
+def run_cbo_nc(data, target):
     extent = np.arange(len(data))
     bb = BB(0, 0, np.NINF, 0, 0, len(data), data, target, None)
     bb.p0 = p(extent, bb)
@@ -290,11 +423,11 @@ def run_cbo(data, target):
     intent = get_closure(extent, bb)
     extent = get_extent(np.copy(intent), np.copy(extent), bb)
     bb.max_obj = max(impact(extent, bb), bb.max_obj)
-    print("intent")
-    print(intent)
-    print("max obj")
-    print(bb.max_obj)
-    print("\n\n")
+#     print("intent")
+#     print(intent)
+#     print("max obj")
+#     print(bb.max_obj)
+#     print("\n\n")
 
     # check if inital objective value is maximum bound
     if bb.max_obj == bb.bnd0:
@@ -309,7 +442,7 @@ def run_cbo(data, target):
     # starts from last attribute (index dims-1) and decrements
     j = dims-1
 
-    search(np.copy(intent), np.copy(extent), j, bb)
+    search_nc(np.copy(intent), np.copy(extent), j, bb)
 
     return bb
 
@@ -328,50 +461,92 @@ def implied_on(j, ext, bb):
     return True
 
 
-def baseline(target, s, r, extent, i, bb):
+def baseline(target, ext_sizes, query, extent, i, bb):
+    print("query")
+    print(query)
+    print("extent")
+    print(extent)
+    print("max obj")
+    print(bb.max_obj)
     bb.num_nodes += 1
-    num = len(r)
+    num = len(query)
 
-    for j in range(i, num):
-        aug_extent = extent[bb.data[extent, j]]
+    for prop in range(i, num):
+        if bb.max_obj == bb.bnd0:
+            return
+        aug_extent = extent[bb.data[extent, prop]]
         if aug_extent.size == 0:
-           continue
-        if r[j]:
+            continue
+        # proposition is already implied
+        if query[prop] == True:
             continue
         bb.num_candidates += 1
-        _r = r.copy()
-        _r[j] = True
+        aug_query = query.copy()
+        aug_query[prop] = True
+        print("tail augmentation")
+        print(aug_query)
 
         bb.max_obj = max(impact(aug_extent, bb), bb.max_obj)
 
         if bnd(aug_extent, bb) < bb.max_obj:
             continue
 
-        pp = True
-        for k in range(j):
-            if r[k]:
-                _r[k] = True
-            elif len(aug_extent) <= s[k] and implied_on(k, aug_extent, bb):
-                pp = False
+        prefix_pres = True
+        for k in range(prop):
+            if len(aug_extent) <= ext_sizes[k] and implied_on(k, aug_extent, bb):
+                prefix_pres = False
                 break
-        if not pp:
+        if not prefix_pres:
             continue
 
-        for k in range(j + 1, num):
-            if len(aug_extent) <= s[k] and implied_on(k, aug_extent, bb):
-                _r[k] = True
-
-        baseline(target, s, _r, aug_extent, j + 1, bb)
+        for k in range(prop + 1, num):
+            if len(aug_extent) <= ext_sizes[k] and implied_on(k, aug_extent, bb):
+                aug_query[k] = True
+        print("HELLO!")
+        print("aug query")
+        print(aug_query)
+        baseline(target, ext_sizes, aug_query, aug_extent, prop + 1, bb)
 
 
 def run_baseline(data, target):
-    rt = root(data)
+    root_query = root(data)
     extent = np.arange(len(data))
     ext_sizes = np.add.reduce(data)
-    bb = BB(0, 0, np.NINF, 0, len(data), data, target, None)
+    bb = BB(0, 0, np.NINF, 0, 0, len(data), data, target, None)
     bb.p0 = p(extent, bb)
-    baseline(target, ext_sizes, rt, extent, 0, bb)
+    bb.bnd0 = bnd(extent, bb)
+    baseline(target, ext_sizes, root_query, extent, 0, bb)
     return bb
+
+
+def run_cbo_random(num_ds, targets):
+    n = np.random.randint(1000)
+    data = num_ds[n]
+    target = targets[n]
+    return run_cbo(data, target)
+
+def run_cbo_nc_random(num_ds, targets):
+    n = np.random.randint(1000)
+    data = num_ds[n]
+    target = targets[n]
+    return run_cbo_nc(data, target)
+
+def run_baseline_random(bin_ds, targets):
+    n = np.random.randint(1000)
+    data = bin_ds[n]
+    target = targets[n]
+    return run_baseline(data, target)
+    
+def random_results(i, j, alpha):
+    num_d = [None]*1000
+    bin_d = [None]*1000
+    targets = [None]*1000
+    for n in range(1000):
+        num_d[n], targets[n] = rand_disc_num_array(i, j, alpha)
+        bin_d[n] = disc_num_to_bin(num_d[n]).copy()
+    t_bin = timeit.timeit('run_baseline_random(bin_d, targets)')
+    t_num = timeit.timeit('run_cbo_random(num_d, targets)')
+    return t_num, t_bin
 
 
 # driver code
@@ -419,23 +594,17 @@ if __name__ == '__main__':
     print(result.num_patterns)
     """
     
-    num_d = np.array([[1,3], [2,2], [3,1]])
-    print("numd", num_d)
-    # bin_d = disc_num_to_bin(num_d)
-    # print("bind", bin_d)
+    num_d = np.array([[1,3],[2,2],[3,1]])
     target = np.array([1,1,0])
-    # result = run_baseline(bin_d, target)
-    # print("target")
-    # print(target)
-    # print("\n")
-    # print(f"data_bin")
-    # print(bin_d)
+    print("target")
+    print(target)
+    #print("numd", num_d)
+    # result = run_cbo(num_d, target)
     # print("max obj")
     # print(result.max_obj)
 
-    result = run_cbo(num_d, target)
-    print(f"data_num")
-    print(num_d)
+    bin_d = disc_num_to_bin(num_d)
+    print("bind", bin_d)
+    result = run_baseline(bin_d, target)
     print("max obj")
     print(result.max_obj)
-    
